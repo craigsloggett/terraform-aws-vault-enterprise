414aa2512c9f8c554236f0ba027afd9a33bf4643
Craig Sloggett <craigsloggett@gmail.com>
2026-04-06 17:28:32 -0400

    fix: Refactor cloud-init.sh script (#37)

--- main.tf
+++ main.tf
@@ -9,31 +9,3 @@ data "aws_vpc" "existing" {
   count = var.existing_vpc != null ? 1 : 0
   id    = var.existing_vpc.vpc_id
 }
-
-locals {
-  vault_fqdn        = "${var.vault_subdomain}.${var.route53_zone.name}"
-  vault_node_count  = 3
-  azs               = slice(data.aws_availability_zones.available.names, 0, 3)
-  cluster_tag_key   = "vault-cluster"
-  cluster_tag_value = var.project_name
-  ebs_device_name   = "/dev/xvdf" # AWS convention for the first additional EBS volume
-
-  config_snapshot_json = templatefile("${path.module}/templates/snapshot.json.tftpl", {
-    aws_s3_bucket = aws_s3_bucket.vault_snapshots.id
-    aws_s3_region = data.aws_region.current.region
-    interval      = var.vault_snapshot_interval
-    retain        = var.vault_snapshot_retain
-  })
-
-  vpc = var.existing_vpc != null ? {
-    id                 = var.existing_vpc.vpc_id
-    cidr               = data.aws_vpc.existing[0].cidr_block
-    private_subnet_ids = var.existing_vpc.private_subnet_ids
-    public_subnet_ids  = var.existing_vpc.public_subnet_ids
-    } : {
-    id                 = module.vpc[0].vpc_id
-    cidr               = var.vpc_cidr
-    private_subnet_ids = module.vpc[0].private_subnets
-    public_subnet_ids  = module.vpc[0].public_subnets
-  }
-}
