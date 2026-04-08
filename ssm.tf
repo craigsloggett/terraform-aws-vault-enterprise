17934c08059e349ade6485cb5dcb2768fea1e6cb
Craig Sloggett <craigsloggett@gmail.com>
2026-04-08 15:54:44 -0400

    chore: Update descriptions

--- ssm.tf
+++ ssm.tf
@@ -28,7 +28,7 @@ resource "aws_ssm_parameter" "vault_pki_ca_cert" {
   name        = "/${var.project_name}/vault/pki/ca-cert"
   type        = "String"
   value       = "uninitialized"
-  description = "Vault PKI CA certificate PEM — published by bootstrap node after PKI configuration"
+  description = "Vault PKI CA certificate PEM (public)"
 
   lifecycle {
     ignore_changes = [value]
