6384709685f262e0617ecec7e2832f67afbb8375
Craig Sloggett <craigsloggett@gmail.com>
2026-04-13 15:01:04 -0400

    refactor: Hardcode IMDS token TTL and remove imds_token_ttl parameter
    Hardcode the IMDSv2 token TTL to 3600 seconds (1 hour) inside
    get_imds_token and remove the imds_token_ttl variable and parameter
    from all functions and the cloud-init template.

--- files/scripts/aws-helpers.sh
+++ files/scripts/aws-helpers.sh
@@ -6,30 +6,28 @@
 # ---------------------------------------------------------------------------
 
 get_imds_token() {
-  imds_token_ttl="${1}"
   curl -s -X PUT \
-    -H "X-aws-ec2-metadata-token-ttl-seconds: ${imds_token_ttl}" \
+    -H "X-aws-ec2-metadata-token-ttl-seconds: 3600" \
     "http://169.254.169.254/latest/api/token"
 }
 
 get_imds_metadata() {
   path="${1}"
-  imds_token_ttl="${2}"
-  token="$(get_imds_token "${imds_token_ttl}")"
+  token="$(get_imds_token)"
   curl -s -H "X-aws-ec2-metadata-token: ${token}" \
     "http://169.254.169.254/latest/meta-data/${path}"
 }
 
 get_private_ip() {
-  get_imds_metadata "local-ipv4" "${1}"
+  get_imds_metadata "local-ipv4"
 }
 
 get_instance_id() {
-  get_imds_metadata "instance-id" "${1}"
+  get_imds_metadata "instance-id"
 }
 
 get_availability_zone() {
-  get_imds_metadata "placement/availability-zone" "${1}"
+  get_imds_metadata "placement/availability-zone"
 }
 
 # ---------------------------------------------------------------------------
