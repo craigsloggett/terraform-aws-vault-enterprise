06cca59257a6e0400873a961399c6f60c8a44841
Craig Sloggett <craigsloggett@gmail.com>
2026-04-15 10:54:53 -0400

    feat: Add intermediate CA for the Vault cluster

--- ssm.tf
+++ ssm.tf
@@ -1,12 +1,12 @@
-resource "aws_ssm_parameter" "vault_pki_ca_cert" {
-  name        = "/${var.project_name}/vault/pki/ca-cert"
+resource "aws_ssm_parameter" "vault_pki_root_ca_cert" {
+  name        = "/${var.project_name}/vault/pki-root/ca-cert"
   type        = "String"
   value       = "uninitialized"
-  description = "Vault PKI CA certificate PEM (public)"
+  description = "Vault PKI root CA certificate PEM"
 
   lifecycle {
     ignore_changes = [value]
   }
 
-  tags = merge(var.common_tags, { Name = "${var.project_name}-vault-pki-ca-cert" })
+  tags = merge(var.common_tags, { Name = "${var.project_name}-vault-pki-root-ca-cert" })
 }
